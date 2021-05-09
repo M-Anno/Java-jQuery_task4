3067074ae77e3fe004f8639fc2ab927043630e05
EC2 Default User <ec2-user@ip-172-31-12-118.us-east-2.compute.internal>
2021-05-09 01:24:41 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".slider").slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: true,
+    arrows: true,
+    centerMode: true,
+    centerPadding: "20px",
+    pauseOnHover: true,
+  });
+});
